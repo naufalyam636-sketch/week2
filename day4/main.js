d20a70e9915480dd3b685e0dfed0eaf04ab36a99
yamin <naufalyam636@gmail.com>
2025-09-13 08:32:56 +0700

    tugas selesai

--- day4/main.js
+++ day4/main.js
@@ -3,3 +3,13 @@ import { luasLingkaran } from "./lingkaran.js"; // Ambil alat hitung lingkaran
 
 console.log("Luas Persegi 5x5:", luasPersegi(5)); // Cetak hasil: 25
 console.log("Luas Lingkaran r=7:", luasLingkaran(7)); // Cetak hasil: ~153.94
+
+import { besar } from "./teks.js";
+
+console.log(besar("belajar modular"));
+import { jamSekarang } from "./waktu.js";
+
+console.log("Jam saat ini:", jamSekarang());
+import sapa from './salam.js'; 
+
+console.log(sapa("mas dani")); 
